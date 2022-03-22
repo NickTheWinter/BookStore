00a001da5907de49a35692c7213e7ff28284996b
NickTheWinter <k.zimenkov@mail.ru>
2022-03-22 11:33:42 +0300

    add class Publishers, Persons

--- BookStore/Database.cs
+++ BookStore/Database.cs
@@ -70,4 +70,117 @@ namespace BookStore
             return false;
         }
     }
+    internal class Persons
+    {
+        static List<string> persons = new List<string>();
+        public static bool Add(string name)
+        {
+            if (!persons.Contains(name))
+            {
+                persons.Add(name);
+                return true;
+            }
+            return false;
+        }
+        public static string Read()
+        {
+            string table = "";
+            foreach (string item in persons)
+                table += Templates.Enum(item, persons);
+            return table;
+        }
+        public static string Select(string id, string set)
+        {
+            string table = "";
+            foreach (string item in persons)
+                if (persons.IndexOf(item).ToString().Contains(id) & item.ToLower().Contains(set.ToLower()))
+                    table += Templates.Enum(item, persons);
+            return table;
+        }
+        public static bool Change(string idString, string name)
+        {
+            try
+            {
+                int id = Convert.ToInt32(idString);
+                string item = persons[id];
+                if (item != name)
+                {
+                    persons[id] = name;
+                    return true;
+                }
+            }
+            catch (FormatException) { }
+            catch (ArgumentOutOfRangeException) { }
+            return false;
+        }
+        public static bool Remove(string idString)
+        {
+            try
+            {
+                int id = Convert.ToInt32(idString);
+                persons.RemoveAt(id);
+                return true;
+            }
+            catch (FormatException) { }
+            catch (ArgumentOutOfRangeException) { }
+            return false;
+        }
+    }
+    internal class Publishers
+    {
+        static List<string> publishers = new List<string>();
+        public static bool Add(string name)
+        {
+            if (!publishers.Contains(name))
+            {
+                publishers.Add(name);
+                return true;
+            }
+            return false;
+        }
+        public static string Read()
+        {
+            string table = "";
+            foreach (string item in publishers)
+                table += Templates.Enum(item, publishers);
+            return table;
+        }
+        public static string Select(string id, string set)
+        {
+            string table = "";
+            foreach (string item in publishers)
+                if (publishers.IndexOf(item).ToString().Contains(id) & item.ToLower().Contains(set.ToLower()))
+                    table += Templates.Enum(item, publishers);
+            return table;
+        }
+        public static bool Change(string idString, string name)
+        {
+            try
+            {
+                int id = Convert.ToInt32(idString);
+                string item = publishers[id];
+                if (item != name)
+                {
+                    publishers[id] = name;
+                    return true;
+                }
+            }
+            catch (FormatException) { }
+            catch (ArgumentOutOfRangeException) { }
+            return false;
+        }
+        public static bool Remove(string idString)
+        {
+            try
+            {
+                int id = Convert.ToInt32(idString);
+                publishers.RemoveAt(id);
+                return true;
+            }
+            catch (FormatException) { }
+            catch (ArgumentOutOfRangeException) { }
+            return false;
+        }
+    }
+
 }
